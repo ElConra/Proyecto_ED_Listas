f9e1fa1065a5c237c9437a56e097ce7edc8a5124
ElConra <qcconrado@gmail.com>
2023-02-27 22:31:25 -0700

    Creando Listas y Nodos en DoblementeCirculares

---
+++ ListasDoblementeCirculares/Lista.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadas
+{
+    internal class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+
+
+        }
+
+
+        public void AgregarFinal(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nodoNuevo = new Nodo(valor, nodoActual);
+            nodoActual.Siguiente = nodoNuevo;
+        }
+
+        public void AgregarInicio(string valor)
+        {
+            if (ValidaVacio())
+            {
+                AgregarFinal(valor);
+                return;
+            }
+            Nodo nuevoNodo = new Nodo(valor, nodoInicial,nodoInicial.Siguiente);
+            nodoInicial.Siguiente.Anterior = nuevoNodo;
+            nodoInicial.Siguiente = nuevoNodo;
+        }
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio())
+            {
+                return null;
+            }
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                if (nodoActual.Valor == valor)
+                {
+                    return nodoActual;
+                }
+
+            }
+            return null;
+        }
+
+
+
+        public Nodo BuscarAnterior(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+
+                while (nodoBusqueda.Siguiente != null
+                            && nodoBusqueda.Siguiente.Valor != valor)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    if (nodoBusqueda.Siguiente.Valor == valor)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        public void BorrarNodo(string valor)
+        {
+
+                nodoActual = Buscar(valor);
+
+                if (nodoActual != null)
+                {
+                nodoActual.Anterior.Siguiente = nodoActual.Siguiente;
+                if (nodoActual.Siguiente != null)
+                {
+                    nodoActual.Siguiente.Anterior = nodoActual.Anterior;
+                }
+                    nodoActual.Siguiente=null;
+                    nodoActual.Anterior=null;
+                }
+            
+        }
+    }
+}
---
+++ ListasDoblementeCirculares/Nodo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeCirculares
+{
+    internal class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo Siguiente { get; set; }
+
+        public Nodo Anterior { get; set; }
+
+        public Nodo(string valor = "", Nodo siguiente = null, Nodo anterior = null)
+        {
+            Valor = valor;
+            Siguiente = siguiente;
+            Anterior = anterior;
+
+        }
+
+    }
+}
